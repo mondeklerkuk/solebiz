aeaed37f6653e3aa552ed515dfb10003c55435b5
SoleBiz Deploy <deploy@solebiz.app>
2026-06-06 13:57:29 +0000

    Fix JobModal bad import - deploy Sourcing + DocumentViewer

--- src/pages/jobs/JobModal.tsx
+++ src/pages/jobs/JobModal.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { supabase } from '../../lib/supabase';
-import { Job, Client, JobStatus, STATUS_LABELS } from './JobsPage';
+import { STATUS_LABELS } from './JobsPage';
 
 interface Props {
   job: Job | null;
